9841ed60199c285c6c11375363e96b712afe72a7
Dorothy Beene <tengallonhatz@gmail.com>
2023-10-27 20:25:33 -0400

    add blue globe. Still need shaders and ambient light effects.

--- template/src/World/World.js
+++ template/src/World/World.js
@@ -1,17 +1,56 @@
+import * as THREE from 'three';
 import { createCamera } from "./Components/camera";
 import { createRenderer } from "./System/renderer";
 import { createScene } from "./Components/scene";
 import { Loop } from "./System/Loop";
 import { Resizer } from "./System/Resizer";
-//import { CreateBaseCube } from "./Components/StarterCube";
-//import { AxesHelper } from "three";
 import { createOrbitControls } from "./Components/OrbitController";
-import { Mesh, SphereGeometry, TextureLoader, MeshBasicMaterial, Scene, Color, Fog } from 'three';
+import { Mesh, SphereGeometry, TextureLoader, ShaderMaterial, Color } from 'three';
+
+
+
+const vertexShader = `
+  #ifdef GL_ES
+  precision mediump float;
+  #endif
+
+  uniform float u_time;
+  uniform float u_maxExtrusion;
+
+  void main() {
+    vec3 newPosition = position;
+    if (u_maxExtrusion > 1.0) {
+      newPosition.xyz = newPosition.xyz * u_maxExtrusion + sin(u_time);
+    } else {
+      newPosition.xyz = newPosition.xyz * u_maxExtrusion;
+    }
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(newPosition, 1.0);
+  }
+`;
+
+const fragmentShader = `
+  #ifdef GL_ES
+  precision mediump float;
+  #endif
+
+  uniform float u_time;
+
+  vec3 colorA = vec3(0.196, 0.631, 0.886);
+  vec3 colorB = vec3(0.192, 0.384, 0.498);
+
+  void main() {
+    vec3 color = vec3(0.0);
+    float pct = abs(sin(u_time));
+    color = mix(colorA, colorB, pct);
+    gl_FragColor = vec4(color, 1.0);
+  }
+`;
 
 let renderer;
 let camera;
 let scene;
 let loop;
+let earth; // Declare earth mesh
 
 class World {
   constructor(container) {
@@ -29,16 +68,51 @@ class World {
     // Load the Earth texture map
     const earthTexture = new TextureLoader().load('../countries.webp'); // Update the path to your texture map
 
-    // Create a material with the Earth texture
-    const earthMaterial = new MeshBasicMaterial({ map: earthTexture });
+    // Create a shader material with the Earth texture
+    const earthMaterial = new ShaderMaterial({
+      uniforms: {
+        u_time: { value: .05 },
+        u_maxExtrusion: { value: 1.1 }
+      },
+      vertexShader: vertexShader,
+      fragmentShader: fragmentShader,
+      side: THREE.DoubleSide, // Set the side to DoubleSide or FrontSide
+    });
 
     // Create the Earth mesh
-    const earth = new Mesh(earthGeometry, earthMaterial);
+    earth = new Mesh(earthGeometry, earthMaterial);
     scene.add(earth);
 
-    // Remove the cube and axesHelper
-    // scene.remove(cube);
-    // scene.remove(axesHelper);
+    // Handle mouse interactions
+    container.addEventListener('mousemove', this.handleMouseMove);
+    container.addEventListener('mousedown', this.handleMouseDown);
+    container.addEventListener('mouseup', this.handleMouseUp);
+  }
+
+  handleMouseMove(event) {
+    // Handle mouse move event
+    const raycaster = new THREE.Raycaster();
+    const mouse = new THREE.Vector2();
+    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+    mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+    raycaster.setFromCamera(mouse, camera);
+    const intersects = raycaster.intersectObject(earth);
+
+    if (intersects.length > 0) {
+      // Handle mouse over the globe
+      // Update shader material or perform other interactions
+    }
+  }
+
+  handleMouseDown(event) {
+    // Handle mouse down event
+    // Modify shader material properties
+  }
+
+  handleMouseUp(event) {
+    // Handle mouse up event
+    // Reset shader material properties
   }
 
   render() {
--- template/src/components/BaseCanvas.vue
+++ template/src/components/BaseCanvas.vue
@@ -1,20 +1,56 @@
 <template>
   <div class="base-canvas-body">
-    <div id="scene-container">
-    </div>
+    <div id="scene-container"></div>
   </div>
 </template>
 
 <script>
-import { World } from '@/World/World'
+import { World } from "@/World/World";
+import * as THREE from "three";
+
 export default {
-  name: 'BaseCanvas',
+  name: "BaseCanvas",
+  data() {
+    return {
+      world: null,
+    };
+  },
   mounted() {
-    const container = document.querySelector('#scene-container');
-    const world = new World(container);
-    world.start();
-  }
-}
+    const container = document.querySelector("#scene-container");
+    this.world = new World(container);
+    this.world.start();
+
+    // Add mouse event listeners for interaction
+    container.addEventListener("mousemove", this.onMouseMove);
+    container.addEventListener("mousedown", this.onMouseDown);
+    container.addEventListener("mouseup", this.onMouseUp);
+  },
+  methods: {
+    onMouseMove(event) {
+      const world = this.world;
+      const mouse = new THREE.Vector2();
+      mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+      mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+      world.handleMouseMove(mouse);
+    },
+    onMouseDown() {
+      const world = this.world;
+      world.handleMouseDown();
+    },
+    onMouseUp() {
+      const world = this.world;
+      world.handleMouseUp();
+    },
+  },
+  beforeDestroy() {
+    // Remove event listeners when the component is destroyed
+    const container = document.querySelector("#scene-container");
+    container.removeEventListener("mousemove", this.onMouseMove);
+    container.removeEventListener("mousedown", this.onMouseDown);
+    container.removeEventListener("mouseup", this.onMouseUp);
+  },
+};
 </script>
 
 <style scoped>
@@ -26,3 +62,4 @@ export default {
   top: 0px;
 }
 </style>
+
